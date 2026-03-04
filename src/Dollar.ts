622fe6835934a4919f5b14445a40892aa8a4d388
Your Name <you@example.com>
2026-03-05 06:44:42 +0900

    aaa

---
+++ src/Dollar.ts
@@ -0,0 +1,9 @@
+export class Dollar {
+  amount: number;
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+  times(multiplier: number) {
+    this.amount = this.amount * multiplier;
+  }
+}
